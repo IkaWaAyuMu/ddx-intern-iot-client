5923b01f8b391f99964b63885f091bb5d0f788b4
IkaWaAyuMu <pungpond4450@gmail.com>
2023-09-25 17:11:50 +0700

    Modal for each parameters

---
+++ components/infoModal.tsx
@@ -0,0 +1,78 @@
+import Feather from "@expo/vector-icons/Feather";
+import { Modal, Pressable, Text, View, Image, ImageSourcePropType } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
+
+export default function InfoModal(props: {
+  info: { topic: string; image?: ImageSourcePropType; short: string; long: string };
+  isOpen: boolean;
+  closeCallback: () => void;
+}) {
+  const { info, isOpen, closeCallback } = props;
+
+  return (
+      <Modal transparent={true} visible={isOpen} onRequestClose={closeCallback} animationType="fade">
+        <Pressable style={styles.screen} onPress={closeCallback}></Pressable>
+        <View style={styles.modalContainer}>
+          <View style={styles.modal}>
+            {info.image && <Image style={styles.imageContainer} source={info.image}/>}
+            <View style={styles.textContainer}>
+              <Text style={{ ...styles.modalText, ...styles.modalHeaderText }}>{info.topic}</Text>
+              <Text style={{ ...styles.modalText, ...styles.modalDescriptionText }}>{info.short}</Text>
+              <Pressable><Text style={{ ...styles.modalText, ...styles.seeMoreText }}>See more -{">"}</Text></Pressable>
+            </View>
+            <Pressable style={styles.closeButton} onPress={closeCallback}><Feather name="x" size={16} color="#fff"/></Pressable>
+          </View>
+        </View>
+      </Modal>
+  );
+}
+
+const styles = EStyleSheet.create({
+  modalContainer: {
+    ...EStyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modal: {
+    overflow: "hidden",
+    borderRadius: "0.75rem",
+    marginHorizontal: "1rem",
+    backgroundColor: "$gray600",
+  },
+  imageContainer: {
+    height: "8.25rem"
+  },
+  textContainer: {
+    padding: "1rem",
+    gap: "0.25rem",
+  },
+  modalText: {
+    color: "$white",
+  },
+  modalHeaderText:{
+    fontFamily: "UberMove-Bold",
+    fontSize: "1.25rem",
+  },
+  modalDescriptionText: {
+    fontFamily: "UberMoveText-Light",
+    fontSize: "0.875rem",
+  },
+  seeMoreText: {
+    fontFamily: "UberMoveText-Medium",
+    fontSize: "0.875rem",
+    marginTop: "0.5rem",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 6,
+    right: 6,
+    borderRadius: "$infinity",
+    backgroundColor: "$black_20",
+  },
+  screen: {
+    ...EStyleSheet.absoluteFillObject,
+    flex: 1,
+    backgroundColor: "$black_overlay",
+  },
+});
